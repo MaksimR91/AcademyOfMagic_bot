7080d3dac89d6d7d458ec1fafa58b23d56243958
MaksimR91 <mrubanov91@gmail.com>
2025-06-13 16:43:40 +0500

    Add files via upload
    Изменен endpoint

--- logger.py
+++ logger.py
@@ -21,7 +21,7 @@ s3_client = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    endpoint_url="https://storage.yandexcloud.net",
+    endpoint_url="https://s3.kz1.cloud-object-storage.yandexcloud.net",
     region_name=REGION_NAME,
 )
 
